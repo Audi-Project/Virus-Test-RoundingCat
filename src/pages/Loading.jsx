248e6b8edb323fb96cd0c5781b1bf11d1315f2da
zop1234 <zop1234@hanmail.net>
2023-06-05 00:04:47 +0900

    복구

---
+++ src/pages/Loading.jsx
@@ -0,0 +1,27 @@
+import tw from 'tailwind-styled-components';
+import LoadingImg from '../assets/loading/loadingVirus.svg';
+
+export default function Loading() {
+  return (
+    <div className="flex items-center w-[390px] h-[844px] bg-[#D8C5FD]">
+      <Contianer>
+        <div className="mb-20 text-[24px]">진단서를 작성하고 있습니다</div>
+        <div className="animate-bounce">
+          <img src={LoadingImg} alt="loading-virus" />
+        </div>
+        <div className="text-[26px] flex">
+          <span>바이러스 분석 중</span>
+          <div className="animate-typing overflow-hidden whitespace-nowrap">
+            ...
+          </div>
+        </div>
+      </Contianer>
+    </div>
+  );
+}
+
+const Contianer = tw.div`
+flex
+flex-col
+items-center
+`;
