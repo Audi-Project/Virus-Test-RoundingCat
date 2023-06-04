4d4c205106dc4a18c4607690b47e9f95057246e7
seobi-97 <tkfkdrk12@skuniv.ac.kr>
2023-06-04 23:04:40 +0900

    파일 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Result from './pages/Result';
+// import Main from './pages/Main';
 
 function App() {
   return (
